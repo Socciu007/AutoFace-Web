271f6105aba75af5adde61421c18e2c2b0667258
Tiền Vũ <tienvm.oneadx@gmail.com>
2024-02-26 16:22:02 +0700

    add i18n

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 var express = require('express');
+const { I18n } = require('i18n');
+const path = require('path');
 var app = express();
+const cookieParser = require('cookie-parser');
+
+//set language
+app.use(cookieParser());
+const i18n = new I18n({
+  locales: ['en', 'vi'],
+  directory: path.join(__dirname, '/locales'),
+  cookie: 'lang',
+});
+app.use(i18n.init);
+
+app.use('/change-lang/:lang', (req, res) => {
+  res.cookie('lang', req.params.lang, { maxAge: 900000 });
+  console.log(req.params.lang);
+  res.redirect('back');
+});
 // set the view engine to ejs
 app.use(express.static('public'));
 
